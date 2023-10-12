06adf9eb0ed5c72b9ff413dcaf7ea317bfee1e70
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-10-12 18:21:02 +0530

    added filter bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,20 @@
 import { renderListing } from './dom.js';
 
-const getJobData = async () => {
+const renderJobs = async () => {
   try {
     const res = await fetch('../data.json');
     const jobs = await res.json();
-    console.log(jobs);
 
     jobs.forEach((job) => {
       renderListing(job, job.new, job.featured);
     });
+
+    const tagElems = document.querySelectorAll('.tag');
+
+    console.log(tagElems);
   } catch (error) {
     console.log(error);
   }
 };
-getJobData();
+
+renderJobs();
